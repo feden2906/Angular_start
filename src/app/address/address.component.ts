c30f10dd0e8eb1155b451580ac4932b703a4724f
Fedenko Maksym <feden2906@gmail.com>
2021-01-20 22:28:35 +0200

    123

--- src/app/address/address.component.ts
+++ src/app/address/address.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-address',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address.component.css']
 })
 export class AddressComponent implements OnInit {
+  @Input()
+  address: any;
 
   constructor() { }
 
